523bf017acb0ccf30e64a322ceb14d5af63c9933
OptimusPrime691 <lucasbmarques8@gmail.com>
2026-03-30 12:30:41 -0300

    Add files via upload
    Soma de 0 até N com Função

---
+++ LucasHumberto-LucasBandeira/Php's/ex8/index.php
@@ -0,0 +1,15 @@
+<!--
+Data: 30/03/2026
+Autor: Lucas Humberto e Lucas Bandeira
+Objetivo:
+Somar números de 0 até N
+-->
+
+<form action="processa.php" method="post">
+
+Número:
+<input type="number" name="n">
+
+<input type="submit">
+
+</form>
