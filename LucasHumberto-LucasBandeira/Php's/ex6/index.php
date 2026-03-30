c0614337ecb1bbe291337777fc34cbfd1e7f7a97
OptimusPrime691 <lucasbmarques8@gmail.com>
2026-03-30 12:27:54 -0300

    Add files via upload
    Série de Fibonacci

---
+++ LucasHumberto-LucasBandeira/Php's/ex6/index.php
@@ -0,0 +1,15 @@
+<!--
+Data: 30/03/2026
+Autor: Lucas Humberto e Lucas Bandeira
+Objetivo:
+Mostrar sequência de Fibonacci
+-->
+
+<form action="processa.php" method="post">
+
+Digite n:
+<input type="number" name="n">
+
+<input type="submit">
+
+</form>
